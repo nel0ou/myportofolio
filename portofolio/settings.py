daff947c6405d2d00ea681e52c625ac725df74b3
nel0ou <nailasalsabilaj@gmail.com>
2026-09-01 15:51:13 +0700

    Selesai Tutorial 01: Fix settings dan PWS deployment

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -127,8 +127,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 WHITENOISE_USE_FINDERS = True
 
 
